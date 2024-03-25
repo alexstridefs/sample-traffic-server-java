ef16a3779bc7431ad3bf611299c75c652dea4823
Alex Stride <alex.stride@featurespace.co.uk>
2024-03-25 14:18:17 +0000

    Slightly refactoring TrafficService

--- src/main/java/org/example/model/Config.java
+++ src/main/java/org/example/model/Config.java
@@ -2,11 +2,11 @@ package org.example.model;
 
 public class Config {
     private String url;
-    private int rate;
+    private int callsPerSecond;
 
     // Getters and Setters
     public String getUrl() { return url; }
     public void setUrl(String url) { this.url = url; }
-    public int getRate() { return rate; }
-    public void setRate(int rate) { this.rate = rate; }
+    public int getCallsPerSecond() { return callsPerSecond; }
+    public void setCallsPerSecond(int callsPerSecond) { this.callsPerSecond = callsPerSecond; }
 }
--- src/main/java/org/example/service/TrafficService.java
+++ src/main/java/org/example/service/TrafficService.java
@@ -7,28 +7,28 @@ import org.springframework.stereotype.Service;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+
 
 @Service
 public class TrafficService {
-    private Config config;
-    private final AtomicBoolean sendTraffic = new AtomicBoolean(false);
+    private Config config = null;
     private ScheduledExecutorService scheduler;
 
-    public synchronized void updateConfig(Config config) {
-        this.config = config;
-        if (sendTraffic.getAndSet(true)) {
+    public synchronized void updateConfig(Config newConfig) {
+        boolean previousConfigPresent = config != null;
+        config = newConfig;
+        if (previousConfigPresent) {
             // If traffic is already being sent, restart with new config
             stopTraffic();
             startTraffic();
         } else {
             startTraffic();
         }
+
     }
 
     public synchronized void deleteConfig() {
         stopTraffic();
-        sendTraffic.set(false);
         config = null;
     }
 
@@ -37,15 +37,17 @@ public class TrafficService {
     }
 
     private void startTraffic() {
-        if (config != null && config.getRate() > 0) {
-            scheduler = Executors.newSingleThreadScheduledExecutor();
-            scheduler.scheduleAtFixedRate(() -> {
-                System.out.println("Sending a request to " + config.getUrl());
-                RemoteEndpoint.EndpointCallResult result = RemoteEndpoint.call(config.getUrl());
-                if (result.statusCode() != 200) {
-                    System.out.println("Status code " + result.statusCode() + " observed!");
-                }
-            }, 0, 1, TimeUnit.SECONDS); // Adjust the third argument based on your rate logic
+        if (config != null) {
+            if (config.getCallsPerSecond() > 0) {
+                scheduler = Executors.newSingleThreadScheduledExecutor();
+                scheduler.scheduleAtFixedRate(() -> {
+                    System.out.println("Sending a request to " + config.getUrl());
+                    RemoteEndpoint.EndpointCallResult result = RemoteEndpoint.call(config.getUrl());
+                    if (result.statusCode() != 200) {
+                        System.out.println("Status code " + result.statusCode() + " observed!");
+                    }
+                }, 0, 1000 / config.getCallsPerSecond(), TimeUnit.MILLISECONDS);
+            }
         }
     }
 
